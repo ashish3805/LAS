30e0d4303814f27801088c3668c4e677fbea6576
Apurvam18 <apurvam18@outlook.com>
2016-09-11 12:26:18 +0530

    passport

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,11 @@
 var express=require('express');
 var passport=require('passport');
+var localStrategy=require('passport-local').Strategy;
 var router=express.Router();
+var user={
+			username:'username',
+			password:'password'
+		}
 
 router.route('/')
 	.post(function (req,res) {
@@ -8,6 +13,20 @@ router.route('/')
 	})
 	.get(function (req,res) {
 		res.send(req.headers);
+		passport.use(new localStrategy(function (username,password,done) {
+										findUser(username,function (err,user) {
+											if(err){
+													console.log(err);
+											}
+											if(!user){
+													console.log("user not found");
+											}
+											if(password==user.password){
+													console.log("success");
+											}
+											})	
+									}
+			))
 		
 	})
 	module.exports=router;
